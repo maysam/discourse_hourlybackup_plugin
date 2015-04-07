18cef86bddd64ebb23eaa8045d77692328b9a5b5
fredericmalo <fredericmalo@gmail.com>
2015-04-07 23:12:17 +0200

    test

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,17 @@
 
 after_initialize do
 
-  load File.expand_path("./app/jobs/hourlybackup.rb", __FILE__)
+
+module Jobs
+  class HourlyBackup < ::Jobs::Scheduled
+    every 5.minutes 
+    sidekiq_options retry: false
+
+    def execute(args)
+      Jobs.enqueue_in(0, :create_daily_backup)
+    end
+  end
+end
+
 
 end
