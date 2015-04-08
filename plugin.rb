b3a8a7c740d5820de107905a1d3cb4366c5377f3
fredericmalo <fredericmalo@gmail.com>
2015-04-08 22:08:51 +0200

    Syntax Test has_something_changed_since

--- plugin.rb
+++ plugin.rb
@@ -11,6 +11,12 @@ after_initialize do
       every 60.minutes
       sidekiq_options retry: false
 
+      def has_something_changed_since?(date=1.hour.ago)
+        [User, Post, Topic].each do |klass|
+          return true if klass.where("created_at >= :date OR updated_at >= :date", date: date).exists?
+        end
+        false
+      end
 
       def execute(args)
         return unless SiteSetting.backup_daily?
