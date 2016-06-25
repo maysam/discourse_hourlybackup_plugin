66ecdd5d789e96c8bf71985d982de8da64a19770
maysam <kherad@gmail.com>
2016-06-26 00:57:12 +0430

    suspend instead of deactivate

--- jobs/unsubscribe_expired_users.rb
+++ jobs/unsubscribe_expired_users.rb
@@ -5,13 +5,31 @@ module Jobs
 
     def execute(args)
       p "Unsubscribing Expired Users"
-      User.all.find_each do |user|
+      User.not_suspended.find_each do |user|
         begin
           if user.custom_fields["user_field_1"]
             if user.custom_fields["user_field_1"].to_date < Time.now
-              user.deactivate
-            else
-              user.activate
+              user.suspended_till = 100.years.from_now
+              user.suspended_at = DateTime.now
+              user.save!
+              user.revoke_api_key
+              StaffActionLogger.new(user).log_user_suspend(user, 'Subscription Expired!')
+              MessageBus.publish "/logout", user.id, user_ids: [user.id]
+            end
+          end
+        rescue => e
+          p e.message
+        end
+      end
+      p "Subscribing Unexpired Users"
+      User.suspended.find_each do |user|
+        begin
+          if user.custom_fields["user_field_1"]
+            if user.custom_fields["user_field_1"].to_date >= Time.now
+              user.suspended_till = nil
+              user.suspended_at = nil
+              user.save!
+              StaffActionLogger.new(user).log_user_unsuspend(user)
             end
           end
         rescue => e
